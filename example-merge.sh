9e21075a08f1d626d4225ebd9869cb78001b6eb2
Marcos Barreto <msb5@cin.ufpe.br>
2018-07-03 11:41:33 -0300

    alguns ajustes

--- example-merge.sh
+++ example-merge.sh
@@ -1,14 +1,23 @@
 git checkout master
 git branch -D example-merge
 
+# criar branch
 git checkout -b example-merge
+# criar arquivo
 echo "created on: example-merge" > merge-file.txt
+# add arquivo
 git add merge-file.txt
+# commit
 git commit -m"example-merge: added merge-file.txt"
 
+# alterar branch
 git checkout master
+# criar arquivo
 echo "created on: master" > merge-file.txt
+# add arquivo
 git add merge-file.txt
+# commit
 git commit -m"master: added merge-file.txt"
 
+# merge
 git merge -m "Merged in example-merge" example-merge
